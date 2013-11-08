cac2207e6f72f11895d873775c563822126f2b79
Simon Chiang <schiang@comverge.com>
2013-11-08 10:02:54 -0700

    Allow a config file to specifiy settings

--- lib/timeseries/utils.rb
+++ lib/timeseries/utils.rb
@@ -2,6 +2,8 @@ class Timeseries
   module Utils
     module_function
 
+    CONFIG_FILE_OPTIONS = %w{start_time period n_steps stop_time}
+
     def coerce(obj)
       case obj
       when ActiveSupport::TimeWithZone then obj
@@ -20,6 +22,17 @@ class Timeseries
         lambda {|time| time.iso8601(format) }
       else
         lambda {|time| time.strftime(format) }
+      end
+    end
+
+    def load_options(config_file, options)
+      settings = File.readlines(config_file).map {|line| line.strip.split("\s", 2) }
+      settings.map do |key, value|
+        if CONFIG_FILE_OPTIONS.include?(key)
+          [key.to_sym, value]
+        else
+          raise "unknown option in config file: #{key.inspect} (#{config_file.inspect})"
+        end
       end
     end
   end
