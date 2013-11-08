1870fd99fecd671dd3f40189190c41b1c0b280d6
Simon Chiang <schiang@comverge.com>
2013-11-08 12:21:05 -0700

    Rework -c into a start time continuation option
    This is a hard, hard feature to get quite right.  Lacking the time to do it
    properly I'm cutting down the scope of the feature so that it simply is a start
    time reloader.  Use in place of -s.

--- lib/timeseries/utils.rb
+++ lib/timeseries/utils.rb
@@ -2,8 +2,6 @@ class Timeseries
   module Utils
     module_function
 
-    CONFIG_FILE_OPTIONS = %w{start_time period n_steps stop_time}
-
     def coerce(obj)
       case obj
       when ActiveSupport::TimeWithZone then obj
@@ -22,17 +20,6 @@ class Timeseries
         lambda {|time| time.iso8601(format) }
       else
         lambda {|time| time.strftime(format) }
-      end
-    end
-
-    def load_options(config_file, options)
-      settings = File.readlines(config_file).map {|line| line.strip.split("\s", 2) }
-      settings.map do |key, value|
-        if CONFIG_FILE_OPTIONS.include?(key)
-          [key.to_sym, value]
-        else
-          raise "unknown option in config file: #{key.inspect} (#{config_file.inspect})"
-        end
       end
     end
   end
